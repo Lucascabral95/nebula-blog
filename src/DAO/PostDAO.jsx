f0b7742b824ef3997196da47bedd492e83e0690c
Lucas Cabral <lucassimple@hotmail.com>
2024-11-27 21:04:01 -0300

    Version 1.0.6

--- src/DAO/PostDAO.jsx
+++ src/DAO/PostDAO.jsx
@@ -17,7 +17,8 @@ class PostDAO {
 
     async getPosts() {
         try {
-            const posts = await Post.find({}).populate("author", "name email avatar createdAt");
+            // const posts = await Post.find({}).populate("author", "name email avatar createdAt");
+            const posts = await Post.find({}).populate("author");
             return posts.reverse();
         } catch (error) {
             console.error("Error al obtener los posts:", error);
